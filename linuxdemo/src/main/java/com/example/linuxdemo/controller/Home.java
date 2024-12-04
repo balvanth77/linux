8e74ca964786d7d5389e926db83e0fddccaa3ea3
balvanth <balvanthmangapuram@gmail.com>
2024-12-04 13:50:01 +0530

    first commit

---
+++ linuxdemo/src/main/java/com/example/linuxdemo/controller/Home.java
@@ -0,0 +1,12 @@
+package com.example.linuxdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+    @GetMapping("/")
+    public String home(){
+        return "welcome to linux and aws";
+    }
+}
